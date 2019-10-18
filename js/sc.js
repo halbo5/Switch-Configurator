54939097baeb33cd750c1c681f202828039cf12b
Alain Bolli <alain.bolli@netc.fr>
2019-10-18 16:24:19 +0200

    Revert "déplacement de fonction"
    This reverts commit 0a70ad87bacc717ba63e763868b0487854e98006.

--- js/sc.js
+++ js/sc.js
@@ -10,29 +10,3 @@ function download(filename, text) {
 
   document.body.removeChild(element);
 }
-
-function setvlan(champs,nbports) {
-	if (champs.idvlandata!="" && champs.idvlanvoix!="") {
-		if (eval("champs.idvlanvoix") < eval("champs.idvlandata")) {
-		$( "#message" ).append( "vlan " + champs.idvlanvoix + " enable name \"Voix\"\n" );
-		for (var i = 1; i<= nbports; i++) {
-			if (eval("champs.port"+i) == "voixport"+i) $( "#message" ).append( "vlan " + champs.idvlanvoix + " port default 1/"+i+"\n");
-		}
-		$( "#message" ).append( "vlan " + champs.idvlandata + " enable name \"Data\"\n" );
-		for (var i = 1; i<= nbports; i++) {
-			if (eval("champs.port"+i) == "dataport"+i) $( "#message" ).append( "vlan " + champs.idvlandata + " port default 1/"+i+"\n");
-			if (eval("champs.port"+i) == "mixteport"+i) $( "#message" ).append( "vlan " + champs.idvlandata + " port default 1/"+i+"\n");
-		}
-		}
-		else {
-		$( "#message" ).append( "vlan " + champs.idvlandata + " enable name \"Data\"\n" );
-		for (var i = 1; i<= nbports; i++) {
-			if (eval("champs.port"+i) == "dataport"+i) $( "#message" ).append( "vlan " + champs.idvlandata + " port default 1/"+i+"\n");
-		}
-		$( "#message" ).append( "vlan " + champs.idvlanvoix + " enable name \"Voix\"\n" );
-		for (var i = 1; i<= nbports; i++) {
-			if (eval("champs.port"+i) == "voixport"+i) $( "#message" ).append( "vlan " + champs.idvlanvoix + " port default 1/"+i+"\n");
-		}
-		}
-		}
-}
